05d9519e0806ec1e1102acffb859f609b10dca89
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-23 16:11:33 -0500

    feat(i18n): add language switcher component with vue-i18n integration.

---
+++ src/shared/presentation/components/language-switcher.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {useI18n} from 'vue-i18n';
+const { t, locale, availableLocales } = useI18n();
+</script>
+
+<template>
+  <pv-select-button v-model="locale" :options="availableLocales"/>
+</template>
+
+<style scoped>
+
+</style>
