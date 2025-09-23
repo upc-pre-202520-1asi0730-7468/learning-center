ccdf4bc0c4cc31eeeb7c4a15a1c11f016dda3fdd
Angel Velasquez <pcisavel@upc.edu.pe>
2025-09-23 16:59:58 -0500

    feat(router): add 404 route and page-not-found view with i18n support.

---
+++ src/shared/presentation/views/page-not-found.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {useI18n} from "vue-i18n";
+import {useRouter} from "vue-router";
+
+const { t } = useI18n();
+const router = useRouter();
+const unavailableRoute = router.currentRoute.value.fullPath;
+</script>
+
+<template>
+  <div class="align-content-start justify-content-start m-4">
+    <h1>{{ t('page-not-found.title')}}</h1>
+    <p>{{ t('page-not-found.content', { unavailable-route: unavailableRoute })}}</p>
+    <router-link to="/home">{{ t('page-not-found.go-home')}}</router-link>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
