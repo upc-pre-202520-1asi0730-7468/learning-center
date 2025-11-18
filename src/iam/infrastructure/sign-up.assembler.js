651c3bf3d4576cff3e22ff4b95fe81d94bd59d15
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-18 15:37:57 -0500

    feat(iam): add sign-up assembler and resource classes for handling sign-up responses.

---
+++ src/iam/infrastructure/sign-up.assembler.js
@@ -0,0 +1,11 @@
+import {SignUpResource} from "./sign-up.resource.js";
+
+export class SignUpAssembler {
+    static toResourceFromResponse(response) {
+        if (response.status !== 200) {
+            console.error(`${response.status}, ${response.statusText}`);
+            return null;
+        }
+        return new SignUpResource(response.data);
+    }
+}
