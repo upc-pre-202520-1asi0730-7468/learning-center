35bfb0e16dc248c575845da122a2345c0d011d3c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-18 17:11:35 -0500

    feat(iam): export iam routes for sign-in and sign-up.

--- src/iam/presentation/iam-routes.js
+++ src/iam/presentation/iam-routes.js
@@ -4,4 +4,6 @@ const signUpForm = () => import('./views/sign-up-form.vue');
 const iamRoutes = [
     {path: 'sign-in', name: 'iam-sign-in', component: signInForm, meta: {title: 'Sign-In'}},
     {path: 'sign-up', name: 'iam-sign-up', component: signUpForm, meta: {title: 'Sign-Up'}},
-]
+];
+
+export default iamRoutes;
